acedc805d3a49f3109d1124266e70c58c148390c
zjostyn <jostyncruzquito@gmail.com>
2024-07-19 17:28:31 -0500

    Mensaje de error al crear usuario cambiado

--- src/app/components/public/register/register.component.ts
+++ src/app/components/public/register/register.component.ts
@@ -38,7 +38,7 @@ export class RegisterComponent {
         }, 1500);
       }, 
       (error) => {
-        this.alert_service.showAlert(error.message, "error");
+        this.alert_service.showAlert("El usuario no pudo ser creado!", "error");
       } 
     )
     }
